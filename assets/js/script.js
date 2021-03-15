46ab6146a9d11c94f0363d4422b3f987dec0aab6
Giselle Raposo <raposo_gigi@icloud.com>
2021-03-14 23:39:03 -0400

    still cant figureout whats wrong with my API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,49 @@
 $(document).ready(function() {
 
   var key = 'b35f2282cec646ddcd73e29b68f7d6d3'; 
-  // var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=b35f2282cec646ddcd73e29b68f7d6d3';
+  var baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=b35f2282cec646ddcd73e29b68f7d6d3';
  
-  // fetch(baseURL)
-  // .then((data) => {console.log('response',data.json()) })
-  // .catch((error) => {console.log(error)}) 
+  fetch(baseURL)
+  .then((data) => {console.log('response',data.json()) })
+  .catch((error) => {console.log(error)}) 
   
     var requestCity = async (city) => {
-      var baseURL = 'http://api.openweathermap.org/data/2.5/weather'
-      var query = `?q=${city}appid=${key}`;
+      var baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+      var query = `?q=${city}&appid=${key}`;
     }  
     
     //make fetch call (promise call)
-    var response = await fetch(baseURL+query);
+    var response = await fetch(baseURL + query);
+
+    //promise data
+    var data = await response.json();
+    return data;
    
+    var searchForm = document.querySelector('.search-location');
+    var cityValue = document.querySelector('.search-location input');
+    var currentCity =document.querySelector('.current-city h2');
     
+    updateWeatherDashboard = (city)=>{
+        cityName.textContent = city.name;
+
+    }
+    
+    
+    //add event listener to form
+    searchForm.addEventListener('submit', e =>{
+        e.preventDefault();
+        var citySearched = cityValue.value.trim();
+        console.log(citySearched)
+        searchForm.requestFullscreen();
+
+        requestCity(citySearched)
+        .then((data) => {
+            updateWeatherDashboard(data);
+
+         })
+        .catch((error) => {console.log(error) })
+    })
+
     
     
     
