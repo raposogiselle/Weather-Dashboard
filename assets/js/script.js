15e27edfb8c1e69750bd344d14c3a73646058642
Giselle Raposo <raposo_gigi@icloud.com>
2021-07-30 16:34:51 -0400

    rearranged my html-allowing for dynamic rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,63 @@
-$(document).ready(function() {
-
-  var key = 'b35f2282cec646ddcd73e29b68f7d6d3'; 
-  var baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=Jacksonville&appid=b35f2282cec646ddcd73e29b68f7d6d3';
- 
-  fetch(baseURL)
-  .then((data) => {console.log('response',data.json()) })
-  .catch((error) => {console.log(error)}) 
+// $(document).ready(function() {
   
-    var requestCity = async (city) => {
-      var baseURL = 'https://api.openweathermap.org/data/2.5/weather'
-      var query = `?q=${city}&appid=${key}`;
-    }  
-    
-    //make fetch call (promise call)
-    var response = await fetch(baseURL + query);
+//   var cityname = 'Jacksonville'
+//   var key = 'b35f2282cec646ddcd73e29b68f7d6d3'; 
+//   var baseURL = `https://openweathermap.org/data/2.5/weather?q=$Jacksonville&appid=${key}`
+  
+//   // function currentCity (cityname) {
+//   fetch(baseURL)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+  // } 
+  
+  // currentCity(cityname)
 
-    //promise data
-    var data = await response.json();
-    return data;
+
+
+
+
+
+
+  // fetch(baseURL)
+  // .then((data) => {console.log('response',data.json()) })
+  // .catch((error) => {console.log(error)}) 
+  
+  //   var requestCity = async (city) => {
+  //     var baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+  //     var query = `?q=${city}&appid=${key}`;
+  //   }  
+    
+  //   //make fetch call (promise call)
+  //   var response = await fetch(baseURL + query);
+
+  //   //promise data
+  //   var data = await response.json();
+  //   return data;
    
-    var searchForm = document.querySelector('.search-location');
-    var cityValue = document.querySelector('.search-location input');
-    var currentCity =document.querySelector('.current-city h2');
+  //   var searchForm = document.querySelector('.search-location');
+  //   var cityValue = document.querySelector('.search-location input');
+  //   var currentCity =document.querySelector('.current-city h2');
     
-    updateWeatherDashboard = (city)=>{
-        cityName.textContent = city.name;
+  //   updateWeatherDashboard = (city)=>{
+  //       cityName.textContent = city.name;
 
-    }
+  //   }
     
     
-    //add event listener to form
-    searchForm.addEventListener('submit', e =>{
-        e.preventDefault();
-        var citySearched = cityValue.value.trim();
-        console.log(citySearched)
-        searchForm.requestFullscreen();
+  //   //add event listener to form
+  //   searchForm.addEventListener('submit', e =>{
+  //       e.preventDefault();
+  //       var citySearched = cityValue.value.trim();
+  //       console.log(citySearched)
+  //       searchForm.requestFullscreen();
 
-        requestCity(citySearched)
-        .then((data) => {
-            updateWeatherDashboard(data);
+  //       requestCity(citySearched)
+  //       .then((data) => {
+  //           updateWeatherDashboard(data);
 
-         })
-        .catch((error) => {console.log(error) })
-    })
+  //        })
+  //       .catch((error) => {console.log(error) })
+  //   })
 
     
     
@@ -58,4 +73,4 @@ $(document).ready(function() {
     
     
       
-    })
+  
