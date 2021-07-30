96b1a4d2e790018a321f7bc61f17c68cc46b9fd1
Giselle Raposo <raposo_gigi@icloud.com>
2021-07-30 18:11:18 -0400

    more issues than vogue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-//  renderlast searched city
 renderLast();
 
 // event listener search button
@@ -29,7 +28,7 @@ $("#saved-cities").on("click", function (e) {
 
 
 function getWeatherDetails(city, addButton) {
-    const apikey = "b35f2282cec646ddcd73e29b68f7d6d3";
+    const apikey = "e1fa53d62b7543d4334120a64ec9531f";
     const query = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
 
     // AJAX call to retrieve data
@@ -76,7 +75,7 @@ function getWeatherDetails(city, addButton) {
         );
 
         // api call 5 day weather forecast
-        const apikey = "b35f2282cec646ddcd73e29b68f7d6d3";
+        const apikey = "e1fa53d62b7543d4334120a64ec9531f";
         const queryTwo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apikey}`;
 
         $.ajax({
@@ -150,79 +149,3 @@ function renderLast(cityOnload) {
 
 
 
-// $(document).ready(function() {
-  
-//   var cityname = 'Jacksonville'
-//   var key = 'b35f2282cec646ddcd73e29b68f7d6d3'; 
-//   var baseURL = `https://openweathermap.org/data/2.5/weather?q=$Jacksonville&appid=${key}`
-  
-//   // function currentCity (cityname) {
-//   fetch(baseURL)
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-  // } 
-  
-  // currentCity(cityname)
-
-
-
-
-
-
-
-  // fetch(baseURL)
-  // .then((data) => {console.log('response',data.json()) })
-  // .catch((error) => {console.log(error)}) 
-  
-  //   var requestCity = async (city) => {
-  //     var baseURL = 'https://api.openweathermap.org/data/2.5/weather'
-  //     var query = `?q=${city}&appid=${key}`;
-  //   }  
-    
-  //   //make fetch call (promise call)
-  //   var response = await fetch(baseURL + query);
-
-  //   //promise data
-  //   var data = await response.json();
-  //   return data;
-   
-  //   var searchForm = document.querySelector('.search-location');
-  //   var cityValue = document.querySelector('.search-location input');
-  //   var currentCity =document.querySelector('.current-city h2');
-    
-  //   updateWeatherDashboard = (city)=>{
-  //       cityName.textContent = city.name;
-
-  //   }
-    
-    
-  //   //add event listener to form
-  //   searchForm.addEventListener('submit', e =>{
-  //       e.preventDefault();
-  //       var citySearched = cityValue.value.trim();
-  //       console.log(citySearched)
-  //       searchForm.requestFullscreen();
-
-  //       requestCity(citySearched)
-  //       .then((data) => {
-  //           updateWeatherDashboard(data);
-
-  //        })
-  //       .catch((error) => {console.log(error) })
-  //   })
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-      
-  
